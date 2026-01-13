b3e61e9a7a46f9cee020e3cb9cb541afbcbb5259
Xurshid444 <Zsaidazimova@gmail.com>
2026-01-13 15:29:23 +0500

    lesson 1 change

--- lesson_2/homework/boolean/boolean1.py
+++ lesson_2/homework/boolean/boolean1.py
@@ -2,7 +2,7 @@
 #ikkalasi ham bo'sh emasligini tekshiradigan dastur yozing.
 
 user = input("Foydalanuvchi nomini kiriting: ")
-parol = input("Parolni kiriting: ")
+parol = input("Parolni kiriting:")
 
 if user != "" and parol != "":
     print("Foydalanuvchi nomi va parol qabul qilindi.")
